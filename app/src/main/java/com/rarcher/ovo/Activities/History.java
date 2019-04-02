e8f17d188568d8d37105842dc93e6cfe56a6f186
Rarchers <2553263392@qq.com>
2019-04-02 22:19:15 +0800

    摸鱼完成

--- app/src/main/java/com/rarcher/ovo/Activities/History.java
+++ app/src/main/java/com/rarcher/ovo/Activities/History.java
@@ -15,16 +15,19 @@ import com.rarcher.ovo.View.Been.CardItem;
 import com.rarcher.ovo.model.History_Been;
 
 import java.io.ByteArrayInputStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
-
+/*
+* 显示历史时光胶囊,注意,初始化的时候检测一下时间,如果是今天的不要加入这个历史列表!!!!!!!
+* */
 public class History extends AppCompatActivity {
     private ViewPager mViewPager;
     private LocalDB localDB;
     private CardPagerAdapter mCardAdapter;
     private ShadowTransformer mCardShadowTransformer;
-
-    List<History_Been> history = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,13 +52,24 @@ public class History extends AppCompatActivity {
         localDB = new LocalDB(getApplicationContext(),"History.db",null,2);
         localDB.getWritableDatabase();
     }
-    public  void  query_date(LocalDB dbhelper){
+    public  void  query_date(LocalDB dbhelper) {
         SQLiteDatabase db = dbhelper.getWritableDatabase();
         Cursor cursor = db.query("History", null, null, null, null, null, null);
         if (cursor.moveToFirst()) {
             do {
                 String date = cursor.getString(cursor.getColumnIndex("time"));
                 String context = cursor.getString(cursor.getColumnIndex("context"));
+                //字符串解析为Date对象,用于比较时间
+                /*SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                Date date1 =sdf.parse(date);*/
+
+                /*
+                * 其实还有一种方法,直接对字符床进行比较                  √用这个,不需要去处理异常
+                * date.compareTo();  使用这个方法,就可以比较出时间  例如:
+                *
+                String date = "2017-07-17";
+                System.out.println("compareToBefore1 : "+date.compareTo("2017-06-16"));
+                * */
                 mCardAdapter.addCardItem(new CardItem(date,context));
             }
             while (cursor.moveToNext());
